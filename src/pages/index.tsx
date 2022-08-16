6eb4d7ef598ad0097cd58cd140c2a1d5c2fa7156
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-08-16 12:32:57 +0800

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+import Meta from "../components/Meta";
+import Header from "../components/Header";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Meta />
+      <Header />
+    </>
+  );
+};
+
+export default Home;
