e2a86b178527d60d6b7f362bf4239e1ffbe1eb0f
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-08-23 23:21:49 +0800

    update domain

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://next-sched-n647gj3d2-vn-aj-vngrd.vercel.app/";
+  : "https://nextsched-beta.vercel.app";
