7cadc3e6865b148b2d9e70c09161f7da17d66714
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-08-24 21:36:55 +0800

    domain change

--- config/index.ts
+++ config/index.ts
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://next-sched-beta.vercel.app";
+  : "https://nextsched-beta.vercel.app";
