2566f0ac9556a87b4bb3f38c68e3e48d22883e43
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-08-24 21:31:49 +0800

    update metas and seo

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,9 +10,23 @@ const Layout = ({ children }: any) => {
     <>
       <Head>
         <title>NextSched</title>
+        <meta name="title" content="NextSched" />
         <meta
           name="description"
-          content="Organize your schedule with NextSched"
+          content="Organize your student schedule with NextSched. Currently in beta and more development coming soon."
+        />
+        <meta
+          name="keywords"
+          content="NextSched, Scheduler. College Planner, Planner"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="revisit-after" content="1 days" />
+        <meta name="author" content="Van AJ Vanguardia" />
+        <meta
+          name="google-site-verification"
+          content="jXwUO7i9wLIGvvlFOrOQd7XumxaOjrQzJV0slwIb9Fw"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
