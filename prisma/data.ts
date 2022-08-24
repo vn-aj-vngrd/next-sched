fb1f05c855e604f5b22e22375073db64c6f0ebff
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-08-24 09:01:40 +0800

    update data

--- prisma/data.ts
+++ prisma/data.ts
@@ -1,20 +1,42 @@
-export const schedData = {
-  classes: [
-    {
-      classCode: "TEST-001",
-      instructor: "test",
-      startingRow: 2,
-      timeRange: 20,
-      className: "relative flex mt-px col-start-5",
-      color: "#d69e2e",
-    },
-    {
-      classCode: "TEST-002",
-      instructor: "test",
-      startingRow: 5,
-      timeRange: 20,
-      className: "relative flex mt-px col-start-1",
-      color: "#d69e2e",
-    },
-  ],
-};
+export const schedData = [
+  {
+    classes: [
+      {
+        classCode: "TEST-001",
+        instructor: "test",
+        startingRow: 2,
+        timeRange: 20,
+        className: "relative flex mt-px col-start-5",
+        color: "#d69e2e",
+      },
+      {
+        classCode: "TEST-002",
+        instructor: "test",
+        startingRow: 5,
+        timeRange: 20,
+        className: "relative flex mt-px col-start-1",
+        color: "#d69e2e",
+      },
+    ],
+  },
+  {
+    classes: [
+      {
+        classCode: "TEST-005",
+        instructor: "test",
+        startingRow: 2,
+        timeRange: 20,
+        className: "relative flex mt-px col-start-3",
+        color: "#d69e2e",
+      },
+      {
+        classCode: "TEST-006",
+        instructor: "test",
+        startingRow: 5,
+        timeRange: 20,
+        className: "relative flex mt-px col-start-2",
+        color: "#d69e2e",
+      },
+    ],
+  },
+];
